b62da846c1430933f0ecd8dd55a8be4c8e8693cb
Aamir <aamirasaram2107@gmail.com>
2023-11-22 18:48:37 +0200

    .

--- scripts/heroku-start.cjs
+++ scripts/heroku-start.cjs
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-// Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', 'build')));
-// Root Redirects to the pre-build assets
+// Your static pre-dist assets folder
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+// Root Redirects to the pre-dist assets
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build'));
+  res.sendFile(path.join(__dirname, '..', 'dist'));
 });
-// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+// Any Page Redirects to the pre-dist assets folder index.html that // will load the react app
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist/index.html'));
 });
 app.listen(port, ()=>{
   console.log("Server is running on port: ", port)
