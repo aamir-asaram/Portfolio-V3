22589789d1735431a5d540e9c7a9083b198aae69
Aamir <aamirasaram2107@gmail.com>
2023-11-22 18:56:23 +0200

    Fix spacing

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -3,8 +3,8 @@ import me from './assets/me.png'
 const Landing = () => {
   return (
     <div className="-z-50 relative overflow-hidden w-[90%] h-screen flex flex-col items-center justify-center p-0">
-      <h2 className='fixed top-[38%] lg:top-[20%] font-bold text-black text-6xl md:text-8xl lg:text-[150pt]'>Hi there! I'm</h2>
-      <h1 className="fixed univers font-bold text-black md:text-[100pt] text-8xl lg:text-[150pt]">Aamir</h1>
+      <h2 className='fixed top-[20%] lg:top-[20%] font-bold text-black text-6xl md:text-8xl lg:text-[150pt]'>Hi there! I'm</h2>
+      <h1 className="fixed univers top-[28%] font-bold text-black md:text-[100pt] text-8xl lg:text-[150pt]">Aamir</h1>
       <img src={me} alt="" className='fixed w-96  md:w-1/3 lg:-mt-[5%] lg:w-[500px] bottom-0' />
     </div>
   )
