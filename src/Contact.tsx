cf917c55cb1f8bb043285fbef4fe2e36f16eb411
Aamir <aamirasaram2107@gmail.com>
2023-11-27 10:00:37 +0200

    Add contact headings

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -2,8 +2,9 @@ const Contact = () => {
   return (
       <div id="goto-contact" className="flex flex-col min-h-screen max-w-screen bg-primary pt-20 pl-6">
         <h2 className="text-4xl lg:text-6xl font-semibold">Let's keep in touch!</h2>
-        <p className="text-2xl">Pop me an <a href="mailto:aamirasaram2107@gmail.com" className="text-black hover:underline">email</a></p>
-        <h2>Or connect with me:</h2>
+        <p className="text-2xl lg:text-3xl pt-10 font-medium">Pop me an email:<br /> <a href="mailto:aamirasaram2107@gmail.com" className="text-black font-normal hover:underline">aamirasaram2107@gmail.com</a></p>
+        <h2 className="text-2xl lg:text-3xl pt-10 font-medium">Leave me a message:</h2>
+        <h2 className="text-2xl lg:text-3xl pt-10 font-medium">Or connect with me:</h2>
       </div>
     )
 }
