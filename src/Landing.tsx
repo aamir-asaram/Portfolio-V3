4c1dad49a0d9a890455594153d41a3899cca2b85
Aamir <aamirasaram2107@gmail.com>
2023-11-28 16:19:37 +0200

    Add links

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -5,12 +5,17 @@ const Landing = () => {
   const handleScroll = () => {
     const currentScrollPos = scrollY
     const about = document.getElementById('goto-about')
+    const home = document.getElementById('landing-img')
+    const head1 = document.getElementById('landing-1')
+    const head2 = document.getElementById('landing-2')
     if (about && currentScrollPos >= about.offsetTop) {
-      const home = document.getElementById('landing-img')
       home?.classList.add('hidden')
+      head1?.classList.add('hidden')
+      head2?.classList.add('hidden')
     } else {
-      const home = document.getElementById('landing-img')
       home?.classList.remove('hidden')
+      head1?.classList.remove('hidden')
+      head2?.classList.remove('hidden')
     }
   }
 
@@ -21,8 +26,8 @@ const Landing = () => {
 
   return (
     <div id="goto-home" className="-z-50 relative overflow-hidden w-[90%] h-screen flex flex-col items-center justify-center p-0">
-      <h2 className='fixed top-[20%] lg:top-[8%] font-bold text-black text-6xl md:text-8xl lg:text-[150pt]'>Hi there! I'm</h2>
-      <h1 className="fixed univers top-[28%] font-bold text-black md:text-[100pt] text-8xl lg:text-[150pt]">Aamir</h1>
+      <h2 id="landing-1" className='fixed top-[20%] lg:top-[8%] font-bold text-black text-6xl md:text-8xl lg:text-[150pt]'>Hi there! I'm</h2>
+      <h1 id="landing-2" className="fixed univers top-[28%] font-bold text-black md:text-[100pt] text-8xl lg:text-[150pt]">Aamir</h1>
       <img id="landing-img" src={me} alt="" className='fixed w-96 md:w-1/3 lg:-mt-[5%] lg:w-[500px] bottom-0' />
     </div>
   )
